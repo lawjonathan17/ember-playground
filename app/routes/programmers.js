3333c6cf1e73f0608d46cb3beda5097ea0aaa672
Jonathan <lawjonathan17@gmail.com>
2023-05-15 16:00:26 -0700

    Quick tutorial notes

--- app/routes/programmers.js
+++ app/routes/programmers.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class ProgrammersRoute extends Route {}
+export default class ProgrammersRoute extends Route {
+  model() {
+    return ['Jonathan Law', 'John Doe', 'Hacker Man'];
+  }
+}
